7969e5ef55f0050f3a62cf840871b7c59a64fdee
jurchiks33 <jurisslisans@gmail.com>
2023-12-10 14:06:54 +0100

    adding html for testing purposes

--- play_ground.js
+++ play_ground.js
@@ -1,10 +1,19 @@
-// var subtotal = (13 + 1) * 5;
-// var shipping = 0.5 * (13 + 1);
+var subtotal = (13 + 1) * 5;
+var shipping = 0.5 * (13 + 1);
 
-// var totatl = subtotal + shipping;
+var total = subtotal + shipping;
 
-// console.log(subtotal);
-// console.log(shipping);
-// console.log(totatl);
+var elSub = document.getElementById('subtotal');
+elSub.textContent = subtotal;
+
+var elShip = document.getElementById('shipping');
+elShip.textContent = shipping;
+
+var elTotal = document.getElementById('total');
+elTotal.textContent = total;
+
+console.log(subtotal);
+console.log(shipping);
+console.log(total);
 
 
