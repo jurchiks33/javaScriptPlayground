369fc979286131384ff6162a0d4cf2d0d3ff2c1b
jurchiks33 <jurisslisans@gmail.com>
2023-12-10 13:34:23 +0100

    Arithmetic operators

---
+++ play_ground.js
@@ -0,0 +1,10 @@
+// var subtotal = (13 + 1) * 5;
+// var shipping = 0.5 * (13 + 1);
+
+// var totatl = subtotal + shipping;
+
+// console.log(subtotal);
+// console.log(shipping);
+// console.log(totatl);
+
+
