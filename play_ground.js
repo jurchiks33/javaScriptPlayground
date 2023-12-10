7ca4242b297c97b0646b977b34df0d3b5db01103
jurchiks33 <jurisslisans@gmail.com>
2023-12-10 14:14:26 +0100

    variablo to store numbers

--- play_ground.js
+++ play_ground.js
@@ -1,19 +1,30 @@
-var subtotal = (13 + 1) * 5;
-var shipping = 0.5 * (13 + 1);
+// var subtotal = (13 + 1) * 5;
+// var shipping = 0.5 * (13 + 1);
 
-var total = subtotal + shipping;
+// var total = subtotal + shipping;
 
-var elSub = document.getElementById('subtotal');
-elSub.textContent = subtotal;
+// var elSub = document.getElementById('subtotal');
+// elSub.textContent = subtotal;
 
-var elShip = document.getElementById('shipping');
-elShip.textContent = shipping;
+// var elShip = document.getElementById('shipping');
+// elShip.textContent = shipping;
 
-var elTotal = document.getElementById('total');
-elTotal.textContent = total;
+// var elTotal = document.getElementById('total');
+// elTotal.textContent = total;
 
-console.log(subtotal);
-console.log(shipping);
-console.log(total);
+// console.log(subtotal);
+// console.log(shipping);
+// console.log(total);
 
 
+
+var price;
+var quantity;
+var total;
+
+price = 5;
+quantity = 20;
+total = price * quantity;
+
+var el = document.getElementById('cost');
+el.textContent = '$' + total;
