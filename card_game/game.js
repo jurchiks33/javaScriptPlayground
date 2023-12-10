575ac887dd189755bb01d08698aa6fe5b4bd2d93
jurchiks33 <jurisslisans@gmail.com>
2023-12-10 16:01:08 +0100

    adding cards

--- card_game/game.js
+++ card_game/game.js
@@ -1,6 +1,13 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
+const cardImages = {};
 
 //Canvas setup.
 ctx.fillStyle = 'green';
 ctx.fillRect(0, 0, canvas.clientWidth, canvas.height);
+
+function loadCardImages() {
+    const categories = ['1', '2', '3', '4', '5'];
+
+    
+}
