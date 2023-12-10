74ce06d3b9454a0da1a44e8faa258f4b94b10be5
jurchiks33 <jurisslisans@gmail.com>
2023-12-10 15:58:13 +0100

    adding card

---
+++ card_game/game.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+
+//Canvas setup.
+ctx.fillStyle = 'green';
+ctx.fillRect(0, 0, canvas.clientWidth, canvas.height);
--- play_ground.js
+++ play_ground.js
@@ -18,13 +18,16 @@
 
 
 
-var price;
-var quantity;
-var total;
+// var price;
+// var quantity;
+// var total;
 
-price = 5;
-quantity = 20;
-total = price * quantity;
+// price = 5;
+// quantity = 20;
+// total = price * quantity;
 
-var el = document.getElementById('cost');
-el.textContent = '$' + total;
+// var el = document.getElementById('cost');
+// el.textContent = '$' + total;
+
+
+
