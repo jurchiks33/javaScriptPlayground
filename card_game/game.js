b09a87ac176389b768c85c0cd9100264708e75a4
jurchiks33 <jurisslisans@gmail.com>
2023-12-10 16:05:34 +0100

    work on a card display

--- card_game/game.js
+++ card_game/game.js
@@ -9,5 +9,10 @@ ctx.fillRect(0, 0, canvas.clientWidth, canvas.height);
 function loadCardImages() {
     const categories = ['1', '2', '3', '4', '5'];
 
-    
+    categories.forEach(category => {
+        const image = new Image();
+        image.src = 'images/${category}.png';
+        cardImages[category] = image;
+    });
 }
+
