56d7aae7b4770fc9c41192145866eadef39f23b3
jurchiks33 <jurisslisans@gmail.com>
2023-12-11 12:47:41 +0100

    working on a card stats

---
+++ card_game/gameLogic.js
@@ -0,0 +1,10 @@
+//game logis is stored in this file.
+
+const cardStats = {
+    '1': {attack: 5, health: 10},
+    '2': {attack: 7, health: 8},
+    '3': {attack: 6, health: 9},
+    '4': {attack: 8, health: 7},
+    '5': {attack: 4, health: 11}
+};
+
